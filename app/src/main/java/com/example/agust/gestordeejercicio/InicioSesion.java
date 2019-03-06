021594a7404f88c9a21961ee8ab3a03178151ddd
EdgarMtz00 <edgar.agustin.martinez@gmail.com>
2019-03-05 19:00:38 -0600

    Funciones de interfaz

--- app/src/main/java/com/example/agust/gestordeejercicio/InicioSesion.java
+++ app/src/main/java/com/example/agust/gestordeejercicio/InicioSesion.java
@@ -1,26 +1,34 @@
 package com.example.agust.gestordeejercicio;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class InicioSesion extends AppCompatActivity {
 
+    EditText etCorreo, etContrasena;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_sesion);
+        etContrasena = findViewById(R.id.etContrasena);
+        etCorreo = findViewById(R.id.etCorreo);
     }
 
-    void onClickInicio(View v){
+    public void onClickInicio(View v){
+        String correo, contrasena;
+        correo = etCorreo.getText().toString();
+        contrasena = etContrasena.getText().toString();
 
     }
 
-    void onClickFacebook(View v){
+    public void onClickFacebook(View v){
 
     }
 
-    void onClickRegistro(View v){
-
+    public void onClickRegistro(View v){
+        startActivity( new Intent(this, Registro.class));
     }
 }
--- app/src/main/java/com/example/agust/gestordeejercicio/Registro.java
+++ app/src/main/java/com/example/agust/gestordeejercicio/Registro.java
@@ -3,28 +3,35 @@ package com.example.agust.gestordeejercicio;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class Registro extends AppCompatActivity {
-
+    EditText etContrasena, etCorreo, etPwd, etEstatura, etPeso, etEdad;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        etContrasena = findViewById(R.id.etContrasena);
+        etCorreo = findViewById(R.idetCorreo);
+        etPwd = findViewById(R.id.etPwd);
+        etEstatura = findViewById(R.id.etEstatura);
+        etPeso = findViewById(R.id.etPeso);
+        etEdad = findViewById(R.id.etEdad);
     }
 
-    void onClickRegistro(View v){
+    public void onClickRegistro(View v){
 
     }
 
-    void onClickFacebook(View v){
+    public void onClickFacebook(View v){
 
     }
 
-    void help(View v){
+    public void help(View v){
 
     }
 
-    void terminos(View v){
+    public void terminos(View v){
 
     }
 }
