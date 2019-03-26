8f2916cc24b3d3b7227b2d751bef1a96a41fc464
Edgar Martinez <agustin__mtz00@outlook.com>
2019-03-25 22:50:19 -0600

    Documentacion cronometro

--- app/src/main/java/com/example/agust/gestordeejercicio/Cronometro.java
+++ app/src/main/java/com/example/agust/gestordeejercicio/Cronometro.java
@@ -16,19 +16,31 @@ import android.widget.ProgressBar;
 
 import java.sql.Time;
 
+/**
+ * Cronometro: Clase Controladora de las funciones y vistas del cronometro.
+ * btnPausa: Boton para iniciar y detener el cronometro.
+ * click: Alterna de estado cada que se presiona btnPausa para cambiar entre iniciar y detener el cronometro.
+ * prgReloj: Barra de progreso circular que muestra el avance del cronometro.
+ * progreso: Segundas transcurridos desde que se inicio el cronometro
+ */
+
 public class Cronometro extends Fragment {
     boolean click;
     ProgressBar prgReloj;
     int progreso = 0;
+    Button btnPausa;
 
+    /**
+     * timerProgress: Objeto para realizar conteo de 60 segundos realizando una accion cada segundo
+     * onTick: Cada segundo se aumenta la variable progreso y se actualiza prgReloj con el nuevo valor
+     * onFinish: Reinicia el conteo al acabar
+     */
     CountDownTimer timerProgress = new CountDownTimer(60000, 1000) {
         @Override
         public void onTick(long millisUntilFinished) {
             progreso++;
             prgReloj.setProgress(progreso);
         }
-
-
 
         @Override
         public void onFinish() {
@@ -42,15 +54,16 @@ public class Cronometro extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_cronometro, container, false);
-
-        Button btnPausa = v.findViewById(R.id.btnPausa);
+        btnPausa = v.findViewById(R.id.btnPausa);
         final Chronometer crono = v.findViewById(R.id.crono);
         prgReloj = v.findViewById(R.id.prgReloj);
         click = true;
-
-        prgReloj.setMax(60);
+        prgReloj.setMax(60); //Establece 60 como el valor maximo de prgReloj
 
         btnPausa.setOnClickListener(new View.OnClickListener() {
+            /**
+             * onClick: basado en el valor de click inicia o detiene el cronometro y el metodo timerProgress
+             */
             @Override
             public void onClick(View v) {
                 if(click){
