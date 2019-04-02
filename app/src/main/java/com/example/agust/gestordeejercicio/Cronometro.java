bde8eff6b82c5efc8c80f6c73009c79162b6070d
Epicyax <emmanuel-gf@hotmail.com>
2019-04-01 22:18:21 -0600

    Boton de Pausa/Reanudar agregado

--- app/src/main/java/com/example/agust/gestordeejercicio/Cronometro.java
+++ app/src/main/java/com/example/agust/gestordeejercicio/Cronometro.java
@@ -18,17 +18,21 @@ import java.sql.Time;
 
 /**
  * Cronometro: Clase Controladora de las funciones y vistas del cronometro.
- * btnPausa: Boton para iniciar y detener el cronometro.
- * click: Alterna de estado cada que se presiona btnPausa para cambiar entre iniciar y detener el cronometro.
- * prgReloj: Barra de progreso circular que muestra el avance del cronometro.
- * progreso: Segundas transcurridos desde que se inicio el cronometro
+ * btnInicio: Boton para iniciar y detener el cronometro.
+ * btnPausa: Boton para pausar y reanudar el cronometro.
+ * clickInicio: Alterna de estado cada que se presiona btnInicio para cambiar entre iniciar y detener el cronómetro.
+ * clickPausa: Alterna de estado cada que se presiona btnPausa para cambiar entre pausar y reanudar el cronómetro.
+ * prgReloj: Barra de progreso circular que muestra el avance del cronómetro.
+ * progreso: Segundos transcurridos desde que se inicio el cronómetro.
+ * progresoTemp: Almacena el tiempo transcurrido desde que se inició el cronómetro hasta que se presionó btnPausa.
  */
 
 public class Cronometro extends Fragment {
-    boolean click;
+    boolean clickInicio, clickPausa;
     ProgressBar prgReloj;
     int progreso = 0;
-    Button btnPausa;
+    long progresoTemp=0;
+    Button btnInicio, btnPausa;
 
     /**
      * timerProgress: Objeto para realizar conteo de 60 segundos realizando una accion cada segundo
@@ -38,13 +42,14 @@ public class Cronometro extends Fragment {
     CountDownTimer timerProgress = new CountDownTimer(60000, 1000) {
         @Override
         public void onTick(long millisUntilFinished) {
+            if(progreso==60)
+                progreso=0;
             progreso++;
             prgReloj.setProgress(progreso);
         }
 
         @Override
         public void onFinish() {
-            progreso = 0;
             prgReloj.setProgress(progreso);
             timerProgress.start();
         }
@@ -54,30 +59,58 @@ public class Cronometro extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_cronometro, container, false);
+        btnInicio = v.findViewById(R.id.btnInicio);
         btnPausa = v.findViewById(R.id.btnPausa);
         final Chronometer crono = v.findViewById(R.id.crono);
         prgReloj = v.findViewById(R.id.prgReloj);
-        click = true;
+        clickInicio = true;
+        clickPausa = true;
         prgReloj.setMax(60); //Establece 60 como el valor maximo de prgReloj
 
-        btnPausa.setOnClickListener(new View.OnClickListener() {
+        btnInicio.setOnClickListener(new View.OnClickListener() {
             /**
              * onClick: basado en el valor de click inicia o detiene el cronometro y el metodo timerProgress
              */
             @Override
             public void onClick(View v) {
-                if(click){
+                if(clickInicio){
                     crono.setBase(SystemClock.elapsedRealtime());
                     crono.start();
                     timerProgress.start();
+                    btnInicio.setText("DETENER");
+                    progreso--;
                 }else{
                     crono.stop();
                     timerProgress.cancel();
                     progreso = 0;
+                    progresoTemp = 0;
                     prgReloj.setProgress(progreso);
                     crono.setBase(SystemClock.elapsedRealtime());
+                    btnInicio.setText("INICIAR");
                 }
-                click = !click;
+                clickInicio = !clickInicio;
+            }
+        });
+
+        btnPausa.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(!clickInicio)
+                {
+                    if(clickPausa){
+                        progresoTemp = crono.getBase() - SystemClock.elapsedRealtime();
+                        crono.stop();
+                        timerProgress.cancel();
+                        btnPausa.setText("REANUDAR");
+                    }else{
+                        crono.setBase(SystemClock.elapsedRealtime() + progresoTemp);
+                        crono.start();
+                        timerProgress.start();
+                        btnPausa.setText("PAUSAR");
+                        progreso--;
+                    }
+                    clickPausa = !clickPausa;
+                }
             }
         });
         return v;
